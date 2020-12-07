831d03cd7052af6b48a335a492afdc6a86274e74
dmitrydk <slipin2020@mail.ru>
2020-12-08 00:40:44 +0300

    Add User - Events migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  EMAIL = /\A\w+@\w+\.\w+\z/
+
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+  validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: { with: EMAIL }
 end
