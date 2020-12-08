462c3fa1001de0d244d81a3dc1687d39b246e73d
dmitrydk <slipin2020@mail.ru>
2020-12-08 12:04:29 +0300

    Try to install Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,10 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import '../../assets/stylesheets/application'
+
+import 'bootstrap/dist/js/bootstrap'
 
 require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/activestorage").start()
+// require("channels")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
