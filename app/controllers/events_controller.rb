16f8acaf0dbbf51eb9d4e6190dc9c9916b20cb3d
dmentry <slipin2020@mail.ru>
2020-12-11 13:20:08 +0300

    Add more of Bootstrap

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :datetime, :description)
+      params.require(:event).permit(:title, :address, :datetime, :description)
     end
 end
