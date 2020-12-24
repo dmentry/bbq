de8f7a6a003f37fc771a6fc54209951cdf7e95e5
dmentry <slipin2020@mail.ru>
2020-12-24 23:24:46 +0300

    Create mailer and add MailJet for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,11 +96,21 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   # config.action_mailer.smtp_settings = {
+  # ActionMailer::Base.smtp_settings = {
+  #     :user_name => ENV['SENDGRID_USERNAME'],
+  #     :password => ENV['SENDGRID_PASSWORD'],
+  #     :domain => 'heroku.com',
+  #     :address => 'smtp.sendgrid.net',
+  #     :port => 587,
+  #     :authentication => :plain,
+  #     :enable_starttls_auto => true
+  # }
+
   ActionMailer::Base.smtp_settings = {
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
+      :user_name => ENV['MAILJET_USERNAME'],
+      :password => ENV['MAILJET_PASSWORD'],
       :domain => 'heroku.com',
-      :address => 'smtp.sendgrid.net',
+      :address => 'in-v3.mailjet.com',
       :port => 587,
       :authentication => :plain,
       :enable_starttls_auto => true
