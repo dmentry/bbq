2ed1ef2fe021cb187e271c00a47b0adf0ed5ebd5
dmentry <slipin2020@mail.ru>
2021-01-01 21:36:15 +0300

    Edit validation of unregistered user's email

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,8 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
+  before_validation :email_downcase
+
   validates :event, presence: true
 
   # проверки выполняются только если user не задан (незареганные приглашенные)
@@ -14,12 +16,15 @@ class Subscription < ApplicationRecord
   # для данного event_id один email может использоваться только один раз (если нет юзера, анонимная подписка)
   validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
 
-  validates :user_email, exclusion: { in: User.map(&:email), message: I18n.t('general.email_error') }
-  
+  validates :user_email, exclusion: { in: User.pluck(:email), message: I18n.t(:user_email, :email_error) }
+
   validate :event_owner_subscription?
 
-  # переопределяем метод, если есть юзер, выдаем его имя,
-  # если нет -- дергаем исходный переопределенный метод
+  def email_downcase
+    user_email&.downcase!
+  end
+
+  # переопределяем метод, если есть юзер, выдаем его имя, если нет -- дергаем исходный переопределенный метод
   def user_name
     if user.present?
       user.name
@@ -28,8 +33,7 @@ class Subscription < ApplicationRecord
     end
   end
 
-  # переопределяем метод, если есть юзер, выдаем его email,
-  # если нет -- дергаем исходный переопределенный метод
+  # переопределяем метод, если есть юзер, выдаем его email, если нет -- дергаем исходный переопределенный метод
   def user_email
     if user.present?
       user.email
@@ -40,7 +44,7 @@ class Subscription < ApplicationRecord
 
   def event_owner_subscription?
     if user == event.user
-      errors.add(:event, message: I18n.t('general.subscript_error'))
+      errors.add(:event, message: I18n.t(:user_email, :subscript_error))
     end
   end
 end
