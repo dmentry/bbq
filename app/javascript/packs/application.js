5cd14798f0841702713715d003b9718d0e0c9a96
dmentry <slipin2020@mail.ru>
2021-01-04 16:56:12 +0300

    Fix map.js error in console & use .exists? method in Subscription model validation 2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,6 @@ require("channels")
 import 'bootstrap/dist/js/bootstrap'
 import '../stylesheets/application'
 import './map'
-// import './ekko-lightbox.min'
-// import './ekko-lightbox.min.js'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
