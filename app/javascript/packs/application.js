64182b9444cdc8319e56eebf4aab5b3e826051d3
dmentry <slipin2020@mail.ru>
2021-01-05 14:07:18 +0300

    Fix bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,10 @@ import '../stylesheets/application'
 import './map'
 import './lightbox'
 
-
+var jQuery = require("jquery");
+// import jQuery from "jquery";
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
