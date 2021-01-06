f5f276956f088a9906395521a05cf6440f4e8362
dmentry <slipin2020@mail.ru>
2021-01-06 18:07:41 +0300

    Edit map.js to remove error in console - Not found element with id 'map' on the main page

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,33 +1,40 @@
 ymaps.ready(init);
 var myMap;
 
-        function init() {
-            address = document.getElementById('map').getAttribute('data-address');
+function init() {
 
-            myMap = new ymaps.Map("map", {
-                center: [55.76, 37.64],
-                zoom: 10
-            });
+    mapElement = document.getElementById('map');
 
-            myGeocoder = ymaps.geocode(address);
+    if (!mapElement) {
+        return;
+    }
 
-            myGeocoder.then(
-                function (res) {
-                    coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+    address = document.getElementById('map').getAttribute('data-address');
 
-                    myMap.geoObjects.add(
-                        new ymaps.Placemark(
-                            coordinates,
-                            {iconContent: address},
-                            {preset: 'islands#blueStretchyIcon'}
-                        )
-                    );
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 10
+    });
 
-                    myMap.setCenter(coordinates);
-                    myMap.setZoom(15);
-                }, function (err) {
-                    alert('Ошибка при определении местоположения');
-                }
+    myGeocoder = ymaps.geocode(address);
+
+    myGeocoder.then(
+        function (res) {
+            coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+
+            myMap.geoObjects.add(
+                new ymaps.Placemark(
+                    coordinates,
+                    {iconContent: address},
+                    {preset: 'islands#blueStretchyIcon'}
+                )
             );
+
+            myMap.setCenter(coordinates);
+            myMap.setZoom(15);
+        }, function (err) {
+            alert('Ошибка при определении местоположения');
         }
+    );
+}
 
