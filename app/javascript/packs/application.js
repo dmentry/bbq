9348269d1749c1a89e610e894f5503b2658e5026
dmentry <slipin2020@mail.ru>
2021-01-07 00:23:12 +0300

    Change gallery to lightbox. Add button to delete photo in the gallery. Add method to allow photo deleting to event creator and person who upload #2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,13 +2,13 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+var jQuery = require("jquery");
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
 import 'bootstrap/dist/js/bootstrap'
 import '../stylesheets/application'
 import './map'
 import './gallery'
-
-var jQuery = require("jquery");
-global.$ = global.jQuery = jQuery;
-window.$ = window.jQuery = jQuery;
 
 const images = require.context('../images', true)
