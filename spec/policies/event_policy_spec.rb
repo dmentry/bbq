cb99c3252b64f942e00fbecde808d02a30aaf9a5
dmentry <slipin2020@mail.ru>
2021-01-31 20:11:15 +0300

    Add policies rspec tests 3

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -3,20 +3,43 @@ require 'rails_helper'
 RSpec.describe EventPolicy do
     subject { EventPolicy }
 
-  let!(:user1) { FactoryBot.create(:user) }
-  let!(:user_context1) { UserContext.new(user1, '') }
-  let!(:user2) { FactoryBot.create(:user) }
-  let!(:user_context2) { UserContext.new(user2, '') }
-  let!(:user_with_pin) { FactoryBot.create(:user) }
-  let!(:user_context_with_pin) { UserContext.new(user_with_pin, '1111') }
-  let!(:another_user_with_pin) { FactoryBot.create(:user) }
-  let!(:another_user_context_with_pin) { UserContext.new(another_user_with_pin, '1111') }
+  let(:user1) { FactoryBot.create(:user) }
+  let(:user_context1) { UserContext.new(user1, '') }
 
-  let!(:event1) { FactoryBot.create(:event, user: user1) }
-  let!(:event2) { FactoryBot.create(:event, user: user2) }
-  let!(:event_with_pin) { FactoryBot.create(:event_with_pin, user: user_with_pin) }
+  let(:user2) { FactoryBot.create(:user) }
+  let(:user_context2) { UserContext.new(user2, '') }
+
+  let(:user_with_pin) { FactoryBot.create(:user) }
+  let(:user_context_with_pin) { UserContext.new(user_with_pin, '1111') }
+
+  let(:another_user_with_pin) { FactoryBot.create(:user) }
+  let(:another_user_context_with_pin) { UserContext.new(another_user_with_pin, '1111') }
+
+  let(:user_context_anon_with_pin) { UserContext.new(nil, '1111') }
+
+  let(:user_context_anon_without_pin) { UserContext.new(nil, '') }
+
+  let(:event1) { FactoryBot.create(:event, user: user1) }
+  let(:event2) { FactoryBot.create(:event, user: user2) }
+  let(:event_with_pin) { FactoryBot.create(:event_with_pin, user: user_with_pin) }
 
   context "when user logged in" do
+    context "create event" do
+      permissions :create? do
+        it "has access" do
+          expect(subject).to permit(user_context1)
+        end
+      end
+    end
+
+    context "not user's own event" do
+      permissions :show? do
+        it "have access" do
+          expect(subject).to permit(user_context1, event2)
+        end
+      end
+    end
+
     context "user's own event" do
       permissions :edit?, :update?, :destroy? do
         it "has access" do
@@ -44,7 +67,7 @@ RSpec.describe EventPolicy do
     context "not user's own event with pin" do
       permissions :edit?, :update?, :destroy? do
         it "does not have access" do
-          expect(subject).not_to permit(user_context1, event_with_pin)
+          expect(subject).not_to permit(another_user_context_with_pin, event_with_pin)
         end
       end
     end
@@ -64,10 +87,39 @@ RSpec.describe EventPolicy do
         end
       end
     end
-
-
   end
 
+  context "when user does not logged in" do
+    context "see standard event" do
+      permissions :show? do
+        it "have access" do
+          expect(subject).to permit(nil, event1)
+        end
+      end
+    end
 
+    context "see event with pin" do
+      permissions :show? do
+        it "have access" do
+          expect(subject).to permit(user_context_anon_with_pin, event_with_pin)
+        end
+      end
+    end
 
+    context "not to see event without pin" do
+      permissions :show? do
+        it "does not have access" do
+          expect(subject).not_to permit(user_context_anon_without_pin, event_with_pin)
+        end
+      end
+    end
+
+    context "not to edit event with pin" do
+      permissions :edit?, :update?, :destroy? do
+        it "does not have access" do
+          expect(subject).not_to permit(user_context_anon_with_pin, event_with_pin)
+        end
+      end
+    end
+  end
 end
