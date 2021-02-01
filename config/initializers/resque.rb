bb99c49c4bd95cec5c8b747d962238d20e805573
dmentry <slipin2020@mail.ru>
2021-02-02 02:09:27 +0300

    Add password for redis to credentials

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,3 +1,5 @@
 Resque.logger = Logger.new(Rails.root.join('log', "#{Rails.env}_resque.log"))
 
 Resque.logger.level = Logger::INFO
+
+Resque.redis = Redis.new(password: Rails.application.credentials.dig(Rails.env.to_sym, :redis, :password))
